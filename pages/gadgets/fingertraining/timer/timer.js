b1e76ebff80d01ce3fa7fbb64ca737d2f8837759
罗文宇 <normence@gmail.com>
2017-09-22 16:33:03 +0800

    implement sound alert

--- pages/gadgets/fingertraining/timer/timer.js
+++ pages/gadgets/fingertraining/timer/timer.js
@@ -3,6 +3,17 @@ var timer       // timer received from .setInterval
 var timerCount  // counts of timer
 
 const normTime = (i) => (i > 9 ? String(i) : '0' + i)
+const actConsume = (c) => {
+  if (c.type === 'second') {
+    return 1
+  }
+  if (c.action.includes('引体')) {
+    return 5
+  }
+  if (c.action.includes('举膝')) {
+    return 3
+  }
+}  // 引体每个5s，举膝每个3s
 
 var showNetwork = false
 
@@ -102,7 +113,6 @@ Page({
     this.setData({
       display: '准备',
     })
-    this.playSounds(soundUrls.ready)
     let tempCount = 3
     timer = setInterval(() => {
       if (tempCount === 0) {
@@ -124,16 +134,32 @@ Page({
   },
 
   runTimer() {
+    const { data } = this
+    const { content } = this.data.currentAct
     timer = setInterval(() => {
-      if (timerCount === 0 && !this.data.noMore) {
+      if (timerCount === 0 && !data.noMore) {
+        // 一组到时
         this.setData({
           display: '下一组',
         })
+        this.playSounds(soundUrls.begin)
         timerCount--
       } else if (timerCount < 0) {
+        // 切换到下一组
         clearInterval(timer)
         this.switchSet()
       } else {
+        const breakTime = data.currentAct.break || 0
+        if (timerCount === breakTime && !data.noMore) {
+          // 休息提示音
+          this.playSounds(soundUrls.break)
+        } else if (content.length > 1) {
+          // 第二项动作提示音
+          if (content[0].type !== 'description'
+            && timerCount === 60 - content[0].time * actConsume(content[0])) {
+            this.playSounds(soundUrls.ready)
+          }
+        }
         this.setData({
           display: normTime(timerCount--),
         })
@@ -145,7 +171,6 @@ Page({
     this.setData({
       currentIndex: this.data.currentIndex + 1,
     })
-    console.log(this.data.currentIndex, this.data.length)
     if (this.data.currentIndex === this.data.length) {
       this.handleResetButtonTaped()
       wx.showModal({
@@ -155,6 +180,7 @@ Page({
         confirmText: '',
         confirmColor: '',
       })
+      this.playSounds(soundUrls.finish)
     } else {
       if (this.data.currentIndex === this.data.length - 1) {
         this.setData({
@@ -177,7 +203,7 @@ Page({
       fail: function (res) {
         if (!showNetwork) {
           wx.showModal({
-            title: '提示音',
+            title: '网络情况',
             content: '网络差或无网络，可能无法播放提示音',
             showCancel: false,
           })
